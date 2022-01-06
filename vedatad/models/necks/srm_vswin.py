3a5408167961d1ee0b92f21d06b974bed3f7f295
chengfeng <chengfeng2333@gmail.com>
2022-01-06 03:45:10 -0500

    add pe for ls-attn

--- vedatad/models/necks/srm_vswin.py
+++ vedatad/models/necks/srm_vswin.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 
 from vedacore.misc import build_from_module, registry
+from vedatad.models.modules.positional_encoding import PositionalEncoding
 from vedatad.models.modules.transformer import (
     TransformerEncoder,
     TransformerEncoderLayer,
@@ -30,13 +31,17 @@ class SRMSwin(nn.Module):
         self.with_transformer = False
         if hasattr(srm_cfg, "with_transformer") and srm_cfg["with_transformer"]:
             self.with_transformer = True
-            transformer_encoder_cfg = srm_cfg["transformer"]
-            attn_layer = TransformerEncoderLayer(
-                **transformer_encoder_cfg["encoder_layer"]
-            )
+            trans_cfg = srm_cfg["transformer"]
+            attn_layer = TransformerEncoderLayer(**trans_cfg["encoder_layer"])
             self.encoder = TransformerEncoder(
-                attn_layer, num_layers=transformer_encoder_cfg["num_layers"]
+                attn_layer, num_layers=trans_cfg["num_layers"]
             )
+
+            # positional encoding
+            if hasattr(trans_cfg, "pos_enc"):
+                self.pe = PositionalEncoding(out_channels)
+            else:
+                self.pe = None
 
     def init_weights(self):
         pass
@@ -57,6 +62,8 @@ class SRMSwin(nn.Module):
         x = self.conv2(x)  # [B, C2, D2]
         if self.with_transformer:
             x = x.permute(2, 0, 1)  # [D2, B, C2]
+            if self.pe:
+                x = self.pe(x)
             x = self.encoder(x)
             x = x.permute(1, 2, 0)  # [B, C2, D2]
         return x
